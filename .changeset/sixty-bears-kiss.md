d5d36594a58f094463faa03cc1f6f0f6cdfdda2f
Shirsendu Bairagi <shirsendu2001@gmail.com>
2025-03-28 22:53:12 +0530

    fix(Hero): adjust layout and button alignment for improved responsiveness

---
+++ .changeset/sixty-bears-kiss.md
@@ -0,0 +1,5 @@
+---
+'red-cat-pictures': patch
+---
+
+fix(Hero): adjust layout and button alignment for improved responsiveness
--- app/components/Section/Hero.vue
+++ app/components/Section/Hero.vue
@@ -24,12 +24,12 @@ const emit = defineEmits<{
       :playsinline="true"
       :loop="true"
       preload="metadata" />
-    <div class="relative col-span-3 !col-start-1 row-start-1 flex flex-col gap-6 text-center lg:col-span-2 lg:text-left">
+    <div class="relative col-span-3 !col-start-1 row-start-1 flex flex-col gap-6 text-center md:row-start-2 lg:col-span-2 lg:text-left">
       <h1 class="-mb-1 text-3xl font-semi-bold md:text-5xl">Elevate Your <br /><span class="rounded bg-primary-500 px-2 py-2">Brand</span> Image</h1>
-      <p class="mx-auto max-w-screen-sm text-lg leading-9 tracking-wide md:text-xl lg:mx-0 lg:mb-8">Nurture the essence of your product with our photography & videography services</p>
-      <ButtonCTA class="hidden lg:flex" @click="emit('contact')" />
+      <p class="mx-auto max-w-screen-sm text-lg leading-9 tracking-wide md:text-xl lg:mx-0">Nurture the essence of your product with our photography & videography services</p>
+      <ButtonCTA class="hidden self-start lg:flex" @click="emit('contact')" />
     </div>
-    <div class="relative bottom-[5rem] col-span-full col-start-1 row-start-4 flex flex-col items-center gap-4 self-end">
+    <div class="relative bottom-[5rem] col-span-full col-start-1 row-start-4 flex flex-col items-center gap-4 self-end md:bottom-16">
       <ButtonCTA class="items-center justify-self-center lg:hidden" @click="emit('contact')" />
       <BrandSlider />
     </div>
