01f9d52550d78352b4c73b0bd39df48b8a2902c5
shba007 <shirsendu2001@gmail.com>
2025-04-26 13:30:04 +0530

    style: fixed content page brightness issue

--- app/pages/blog/[...slug].vue
+++ app/pages/blog/[...slug].vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
 const route = useRoute()
 const slug = route.params.slug!.toString()
-const { data: episode } = await useFetch<ContentDetails>(`/api/episode/${slug}`)
+const { data: blog } = await useFetch<ContentDetails>(`/api/blog/${slug}`)
 
-if (!episode.value) {
+if (!blog.value) {
   throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
 }
 
-if (episode.value.url !== '/episode/' + slug) {
-  await navigateTo(episode.value.url, { redirectCode: 301 })
+if (blog.value.url !== '/blog/' + slug) {
+  await navigateTo(blog.value.url, { redirectCode: 301 })
 }
 
-const title = `${episode.value.title}`
-const description = `${episode.value.description}`
+const title = `${blog.value.title}`
+const description = `${blog.value.description}`
 const url = 'https://redcatpictures.com'
-const imageUrl = `https://ucarecdn.com/${episode.value.cover}/-/format/auto/-/preview/1200x630/center`
+const imageUrl = `https://ucarecdn.com/${blog.value.cover}/-/format/auto/-/preview/1200x630/center`
 
 useSeoMeta({
   title: title,
@@ -33,19 +33,19 @@ useSchemaOrg([
     headline: title,
     description: description,
     thumbnailUrl: imageUrl,
-    datePublished: new Date(episode.value.publishedAt).toISOString(),
-    dateModified: new Date(episode.value.modifiedAt).toISOString(),
+    datePublished: new Date(blog.value.publishedAt).toISOString(),
+    dateModified: new Date(blog.value.modifiedAt).toISOString(),
     keywords: [],
   }),
 ])
 </script>
 
 <template>
-  <article v-if="episode" class="w-full">
+  <article v-if="blog" class="w-full">
     <NuxtImg
       provider="uploadcare"
-      :src="episode.cover"
-      :alt="episode.title"
+      :src="blog.cover"
+      :alt="blog.title"
       :width="1280"
       :height="Math.round(1280 / (16 / 9))"
       fit="cover"
@@ -53,15 +53,15 @@ useSchemaOrg([
       class="cover-img absolute left-0 aspect-[5/3] max-h-[20rem] w-screen object-cover" />
     <div class="invisible -left-4 aspect-[5/3] max-h-[20rem] w-screen" />
     <div class="content relative mx-auto max-w-4xl leading-relaxed">
-      <h1 class="mt-4">{{ episode.title }}</h1>
+      <h1 class="mt-4">{{ blog.title }}</h1>
       <div class="mb-2 mt-4 flex justify-between gap-8 text-black/60 dark:text-white/60 md:mt-8">
-        <NuxtTime :datetime="episode.publishedAt" day="numeric" month="short" year="numeric" />
+        <NuxtTime :datetime="blog.publishedAt" day="numeric" month="short" year="numeric" />
         <span class="text-right text-base">
           Updated on
-          <NuxtTime :datetime="episode.modifiedAt" day="numeric" month="short" year="numeric" />
+          <NuxtTime :datetime="blog.modifiedAt" day="numeric" month="short" year="numeric" />
         </span>
       </div>
-      <MarkdownContent :content="episode.content" />
+      <MarkdownContent :content="blog.content" />
     </div>
   </article>
 </template>
